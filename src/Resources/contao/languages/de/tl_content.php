d82581e1001f55b38723672dbe3574bc2afdb647
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2019-07-26 15:27:55 +0200

    Allow to add a wrapper element to the nodes (#13)

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -9,3 +9,5 @@
  */
 
 $GLOBALS['TL_LANG']['tl_content']['nodes'] = ['Nodes', 'Wählen Sie ein oder mehrere Nodes aus.'];
+$GLOBALS['TL_LANG']['tl_content']['nodesWrapper'] = ['Wrapper hinzufügen', 'Einen Wrapper um die ausgewählten Nodes ausgeben.'];
+
--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -9,3 +9,4 @@
  */
 
 $GLOBALS['TL_LANG']['tl_content']['nodes'] = ['Nodes', 'Please choose one or more nodes.'];
+$GLOBALS['TL_LANG']['tl_content']['nodesWrapper'] = ['Add wrapper element', 'Add a wrapper element to the nodes.'];
