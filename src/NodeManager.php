0969e3faaef7bc4ecd9e8552fd1a416141f48924
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2020-07-28 10:34:51 +0200

    Simplify the code

--- src/NodeManager.php
+++ src/NodeManager.php
@@ -83,15 +83,13 @@ class NodeManager
     private function generateBuffer(NodeModel $nodeModel): string
     {
         $buffer = '';
-        $arrElementsData = [];
+        $elementsData = [];
 
         if (null !== ($elements = $nodeModel->getContentElements())) {
             /** @var ContentModel $element */
-            foreach ($elements as $key => $element) {
-                $arrElementsData[$key] = array_merge($element->row(), [
-                    'node_sequence' => $key,
-                ]);
-                $element->setRow($arrElementsData[$key]);
+            foreach ($elements as $index => $element) {
+                $elementsData[] = $element->row();
+                $element->nodeElementIndex = $index;
                 $buffer .= Controller::getContentElement($element);
             }
         }
@@ -102,7 +100,7 @@ class NodeManager
 
         $template = new FrontendTemplate($nodeModel->nodeTpl ?: 'node_default');
         $template->setData($nodeModel->row());
-        $template->arrElementsData = $arrElementsData;
+        $template->elementsData = $elementsData;
 
         $cssID = StringUtil::deserialize($nodeModel->cssID, true);
 
