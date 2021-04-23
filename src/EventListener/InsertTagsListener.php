89828f6b5a0e843dc1d4655e7e4c07cbfe3088ca
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-04-23 12:35:38 +0200

    Log invalid node IDs in insert tag (#24)

--- src/EventListener/InsertTagsListener.php
+++ src/EventListener/InsertTagsListener.php
@@ -12,7 +12,10 @@ declare(strict_types=1);
 
 namespace Terminal42\NodeBundle\EventListener;
 
+use Contao\CoreBundle\Monolog\ContaoContext;
+use Contao\Environment;
 use Contao\StringUtil;
+use Psr\Log\LoggerInterface;
 use Terminal42\NodeBundle\NodeManager;
 
 class InsertTagsListener
@@ -23,11 +26,17 @@ class InsertTagsListener
     private $manager;
 
     /**
+     * @var LoggerInterface|null
+     */
+    private $logger;
+
+    /**
      * InsertTagsListener constructor.
      */
-    public function __construct(NodeManager $manager)
+    public function __construct(NodeManager $manager, LoggerInterface $logger = null)
     {
         $this->manager = $manager;
+        $this->logger = $logger;
     }
 
     /**
@@ -39,27 +48,45 @@ class InsertTagsListener
     {
         $chunks = explode('::', $tag);
 
-        if ('insert_node' === $chunks[0] || 'insert_nodes' === $chunks[0]) {
-            return $this->generateNodes($chunks[1]);
+        if ('insert_node' !== $chunks[0] && 'insert_nodes' !== $chunks[0]) {
+            return false;
         }
 
-        return false;
+        $ids = StringUtil::trimsplit(',', $chunks[1]);
+        $total = \count($ids);
+
+        if (0 === $total) {
+            // Let Contao generate an "unknown insert tag" error in the back end
+            return false;
+        }
+
+        if (1 === $total) {
+            if (null === $buffer = $this->manager->generateSingle((int) $ids[0])) {
+                $this->logError($ids, $tag);
+            }
+
+            return $buffer;
+        }
+
+        $nodes = $this->manager->generateMultiple($ids);
+        $invalid = array_diff_key(array_flip($ids), $nodes);
+
+        if (!empty($invalid)) {
+            $this->logError($invalid, $tag);
+        }
+
+        return implode("\n", $nodes);
     }
 
-    /**
-     * Generate the nodes.
-     */
-    private function generateNodes(string $ids): string
+    private function logError(array $ids, string $tag): void
     {
-        if (0 === \count($ids = StringUtil::trimsplit(',', $ids))) {
-            return '';
+        if (null === $this->logger) {
+            return;
         }
 
-        // Generate a single node
-        if (1 === \count($ids)) {
-            return $this->manager->generateSingle((int) $ids[0]);
-        }
-
-        return implode("\n", $this->manager->generateMultiple($ids));
+        $this->logger->error(
+            'Invalid node(s) '.implode(', ', $ids).' in insert tag ('.$tag.') on page ' . Environment::get('uri'),
+            ['contao' => new ContaoContext(self::class, ContaoContext::ERROR)]
+        );
     }
 }
