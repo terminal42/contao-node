17f3c299e3ff9f05573e5828ce99faf4b1399c17
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-02-23 15:14:48 +0100

    Fix PHP 8 incompatibility in DCA tl_node

--- src/Resources/contao/dca/tl_node.php
+++ src/Resources/contao/dca/tl_node.php
@@ -91,7 +91,7 @@ $GLOBALS['TL_DCA']['tl_node'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_node']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
                 'button_callback' => ['terminal42_node.listener.data_container', 'onDeleteButtonCallback'],
             ],
             'show' => [
