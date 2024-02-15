eea44eb119b0245fc52d53309ceac1f6fbd7ff37
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2024-02-15 20:47:21 +0000

    fix error when loading DI extension

--- src/DependencyInjection/Terminal42NodeExtension.php
+++ src/DependencyInjection/Terminal42NodeExtension.php
@@ -16,7 +16,7 @@ class Terminal42NodeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yml');
 
         if (class_exists(DcaLoaderListener::class)) {
